5b8209788afc36b9497cb9f8f48259250dcca748
aprilia shofi <apriliashofi24@yahoo.com>
2019-01-06 21:20:14 +0700

    [Film] rest crud.

--- application/controllers/Film.php
+++ application/controllers/Film.php
@@ -11,40 +11,45 @@ class Film extends REST_Controller{
   }
 
   // method index untuk menampilkan semua data person menggunakan method get
-  public function index_get(){
-    $response = $this->FilmM->all_film();
+  public function index_get($id = 0)
+  {
+    if ($id > 0)
+    {
+      $response = $this->FilmM->find_by_id($id);
+    }
+    else
+    {
+      $response = $this->FilmM->all_film();
+    }
+
     $this->response($response);
   }
 
   // untuk menambah person menaggunakan method post
-  public function add_post(){
+  public function index_post(){
     $response = $this->FilmM->add_film(
         $this->post('judul'),
         $this->post('tgl'),
         $this->post('ket')
-      );
+    );
     $this->response($response);
   }
 
   // update data person menggunakan method put
-  public function update_put(){
-    $response = $this->PersonM->update_film(
-        $this->put('id_film'),
-        $this->put('judul'),
-        $this->put('tgl'),
-        $this->put('ket')
-      );
+  public function index_put($id = 0){
+    $response = $this->FilmM->update_film(
+      $id,
+      $this->put('judul'),
+      $this->put('tgl'),
+      $this->put('ket')
+    );
     $this->response($response);
   }
 
   // hapus data person menggunakan method delete
-  public function delete_delete(){
-    $response = $this->FilmM->delete_film(
-        $this->delete('id_film')
-      );
+  public function index_delete($id = 0)
+  {
+    $response = $this->FilmM->delete_film($id);
     $this->response($response);
   }
-
 }
-
-?>
--- application/models/FilmM.php
+++ application/models/FilmM.php
@@ -13,31 +13,36 @@ class FilmM extends CI_Model{
 
   // function untuk insert data ke tabel tb_person
   public function add_film($judul,$tgl,$ket){
+    $data = array(
+      "judul"=>$judul,
+      "tgl"=>$tgl,
+      "ket"=>$ket
+    );
 
-    if(empty($judul) || empty($tgl) || empty($ket)){
-      return $this->empty_response();
+    $insert = $this->db->insert("film", $data);
+
+    if($insert){
+      $response['status']=200;
+      $response['error']=false;
+      $response['message']='Data film ditambahkan.';
+      return $response;
     }else{
-      $data = array(
-        "judul"=>$judul,
-        "tgl"=>$tgl,
-        "ket"=>$ket
-      );
-
-      $insert = $this->db->insert("film", $data);
-
-      if($insert){
-        $response['status']=200;
-        $response['error']=false;
-        $response['message']='Data film ditambahkan.';
-        return $response;
-      }else{
-        $response['status']=502;
-        $response['error']=true;
-        $response['message']='Data film gagal ditambahkan.';
-        return $response;
-      }
+      $response['status']=502;
+      $response['error']=true;
+      $response['message']='Data film gagal ditambahkan.';
+      return $response;
     }
+  }
 
+  public function find_by_id($id_film)
+  {
+    $film = $this->db->from('film')->where('id_film', $id_film)->get()->row_array();
+    $response = [
+      'error' => false,
+      'status' => 200,
+      'film' => $film,
+    ];
+    return $response;
   }
 
   // mengambil semua data person
@@ -52,50 +57,36 @@ class FilmM extends CI_Model{
   }
 
   // hapus data person
-  public function delete_film($id_film){
-
-    if($id_film == ''){
-      return $this->empty_response();
-    }else{
-      $where = array(
-        "id_film"=>$id_film
-      );
-
-      $this->db->where($where);
-      $delete = $this->db->delete("film");
-      if($delete){
+  public function delete_film($id_film)
+  {
+      $this->db->where('id_film', $id_film)->delete('film');
+      $rows = $this->db->affected_rows();
+      
+      if ($rows > 0)
+      {
         $response['status']=200;
         $response['error']=false;
         $response['message']='Data film dihapus.';
         return $response;
-      }else{
+      }
+      else
+      {
         $response['status']=502;
         $response['error']=true;
         $response['message']='Data film gagal dihapus.';
         return $response;
       }
-    }
-
   }
 
   // update person
-  public function update_film($id_film,$judul,$tgl,$ket){
-
-    if($id_film == '' || empty($judul) || empty($tgl) || empty($ket)){
-      return $this->empty_response();
-    }else{
-      $where = array(
-        "id_film"=>$id_film
-      );
-
+  public function update_film($id_film, $judul, $tgl, $ket){
       $set = array(
         "judul"=>$judul,
         "tgl"=>$tgl,
         "ket"=>$ket
       );
 
-      $this->db->where($where);
-      $update = $this->db->update("film",$set);
+      $update = $this->db->where('id_film', $id_film)->update("film", $set);
       if($update){
         $response['status']=200;
         $response['error']=false;
@@ -107,10 +98,6 @@ class FilmM extends CI_Model{
         $response['message']='Data film gagal diubah.';
         return $response;
       }
-    }
-
   }
 
 }
-
-?>
